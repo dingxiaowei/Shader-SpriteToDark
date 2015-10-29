111c2fcb9e6b96e7072db92e660cff747fabed8f
dingxiaowei <1213250243@qq.com>
2015-10-29 10:00:38 +0800

    通过改变NGUI的底层shader实现sprite的去色

--- README.md
+++ README.md
@@ -6,4 +6,7 @@ Unity中图片去色<br/>
 Unity版本：4.6.8<br/>
 <a style="color:red">说明:</a>
 ------------------------------------------------
-GrayProject.unitypackage实现原理：通过不同的材质选择不同的shader来处理整个图集去色<br/>
+GrayProject.unitypackage实现原理：<br/>
+方法1：通过不同的材质选择不同的shader来处理整个图集去色<br/>
+方法2：通过设置sprite的ColorR的Value来实现去色
+
--- SpriteToDarkProject/Assets/Scripts/RemoveSpriteColorBySetColorRValue.cs
+++ SpriteToDarkProject/Assets/Scripts/RemoveSpriteColorBySetColorRValue.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RemoveSpriteColorBySetColorRValue : MonoBehaviour {
+public class RemoveSpriteColorBySetColorRValue : MonoBehaviour
+{
 
 	public UISprite Star;
 	private bool isDark = false;
-	
-	void Update () {
-		Debug.LogError("*************");
-		this.Star.color = this.isDark ? new Color(255, 255, 255) : new Color(51, 255, 255);
+
+	void Start()
+	{
+		//闪烁吧，Star！
+		InvokeRepeating("Repeat", 0f, 0.5f);
+	}
+
+	void Repeat()
+	{
+		this.Star.color = this.isDark ? new Color(1f, 1f, 1f, 1f) : new Color(0.2f, 1f, 1f, 1f);
 		this.isDark = !this.isDark;
 	}
 }
